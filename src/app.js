ce8177164f5332dc14e9be4847a85a952e55c3eb
FranciscaMora <fmoraparedes@gmail.com>
2019-02-15 09:24:33 -0300

    2 pantallas listas para deploy y entrega versión 1.1

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 window.onload=()=>{
     document.getElementById('full-info-screen').style.display='none';
+    document.getElementById('unavailable').style.display='none';
 
 }
 
@@ -7,10 +8,18 @@ document.getElementById('down-btn').addEventListener('click',(evento)=>{
     evento.preventDefault();
     document.getElementById('main').style.display='none';
     document.getElementById('full-info-screen').style.display='block';
+    document.getElementById('unavailable').style.display='none';
 });
 
 document.getElementById('logo2').addEventListener('click',(evento)=>{
     evento.preventDefault();
     document.getElementById('full-info-screen').style.display='none';
     document.getElementById('main').style.display='block';
+    document.getElementById('unavailable').style.display='none';
 });
+
+document.getElementById('we').addEventListener('click',(evento)=>{
+    evento.preventDefault();
+    document.getElementById('main').style.display='none';
+    document.getElementById('unavailable').style.display='block';
+})
